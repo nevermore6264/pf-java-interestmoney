c6a2786e1b7b393558d28c2dff80221bd2675bbf
nevermore6264 <37317921+nevermore6264@users.noreply.github.com>
2018-03-21 16:38:54 +0700

    [Thực hành] Ứng dụng tính tiền lãi cho vay

---
+++ src/interest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class interest {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double interset_rate = 1.0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter investment amount:");
+        money = sc.nextDouble();
+        System.out.println("Enter number of months:");
+        month = sc.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interset_rate = sc.nextDouble();
+        double total = 0;
+        for (int i = 0; i < month; i++) {
+            total += money * (interset_rate / 100) / 12 * month;
+        }
+        System.out.println("Total :" + total);
+    }
+}
